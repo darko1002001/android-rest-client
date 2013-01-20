5ffee3bbe1a022cc236990209ac799b75c3ef750
darko <darkogrozdanovski@gmail.com>
2013-01-20 14:01:41 +0100

    Small mods

--- rest-client-lib/src/com/dg/libs/rest/client/BaseRestClient.java
+++ rest-client-lib/src/com/dg/libs/rest/client/BaseRestClient.java
@@ -26,7 +26,7 @@ import com.dg.libs.rest.domain.ResponseStatus;
 public abstract class BaseRestClient implements Rest {
 
     public enum RequestMethod {
-	GET, POST, PUT, DELETE
+        GET, POST, PUT, DELETE
     }
 
     private static final String TAG = ParametersRestClient.class.getSimpleName();
@@ -48,170 +48,169 @@ public abstract class BaseRestClient implements Rest {
     private static AuthenticationProvider authenticationProvider;
 
     public BaseRestClient() {
-	headers = new ArrayList<NameValuePair>();
-	params = new ArrayList<NameValuePair>();
+        headers = new ArrayList<NameValuePair>();
+        params = new ArrayList<NameValuePair>();
+        authenticateRequest();
     }
 
     @Override
     public int getConnectionTimeout() {
-	return connectionTimeout;
+        return connectionTimeout;
     }
 
     @Override
     public void setUrl(final String url) {
-	this.url = url;
+        this.url = url;
     }
 
     @Override
     public String getUrl() {
-	return url;
+        return url;
     }
 
     @Override
     public void setConnectionTimeout(final int connectionTimeout) {
-	this.connectionTimeout = connectionTimeout;
+        this.connectionTimeout = connectionTimeout;
     }
 
     @Override
     public int getSocketTimeout() {
-	return socketTimeout;
+        return socketTimeout;
     }
 
     @Override
     public void setSocketTimeout(final int socketTimeout) {
-	this.socketTimeout = socketTimeout;
+        this.socketTimeout = socketTimeout;
     }
 
     @Override
     public String getResponse() {
-	return response;
+        return response;
     }
 
     @Override
     public ResponseStatus getResponseStatus() {
-	return responseStatus;
+        return responseStatus;
     }
 
     @Override
     public ArrayList<NameValuePair> getHeaders() {
-	return headers;
+        return headers;
     }
 
     @Override
     public ArrayList<NameValuePair> getParams() {
-	return params;
+        return params;
     }
 
     @Override
     public void addHeader(final String name, final String value) {
-	if (TextUtils.isEmpty(value) == false) {
-	    headers.add(new BasicNameValuePair(name, value));
-	}
+        if (TextUtils.isEmpty(value) == false) {
+            headers.add(new BasicNameValuePair(name, value));
+        }
     }
 
     @Override
     public void addParam(final String name, final String value) {
-	if (TextUtils.isEmpty(value) == false) {
-	    params.add(new BasicNameValuePair(name, value));
-	}
+        if (TextUtils.isEmpty(value) == false) {
+            params.add(new BasicNameValuePair(name, value));
+        }
     }
 
     @Override
     public void setAuthentication(AuthenticationProvider authProvider) {
-	this.authProvider = authProvider;
+        this.authProvider = authProvider;
     }
 
     public static void setDefaultAuthenticationProvider(AuthenticationProvider provider) {
-	BaseRestClient.authenticationProvider = provider;
+        BaseRestClient.authenticationProvider = provider;
     }
 
     private void authenticateRequest() {
-	if (authProvider != null) {
-	    authProvider.authenticateRequest(this);
-	    return;
-	}
-	if (authenticationProvider != null) {
-	    authenticationProvider.authenticateRequest(this);
-	    return;
-	}
-	TokenAuthenticationProvider.getInstance().authenticateRequest(this);
+        if (authProvider != null) {
+            authProvider.authenticateRequest(this);
+            return;
+        }
+        if (authenticationProvider != null) {
+            authenticationProvider.authenticateRequest(this);
+            return;
+        }
+        TokenAuthenticationProvider.getInstance().authenticateRequest(this);
     }
 
     @Override
     public void executeRequest(final HttpUriRequest request) throws IOException {
-	authenticateRequest();
+        // add headers
+        for (NameValuePair h : getHeaders()) {
+            request.addHeader(h.getName(), h.getValue());
+        }
 
-	// add headers
-	for (NameValuePair h : getHeaders()) {
-	    request.addHeader(h.getName(), h.getValue());
-	}
+        final HttpParams httpParameters = new BasicHttpParams();
+        // Set the timeout in milliseconds until a connection is established.
+        HttpConnectionParams.setConnectionTimeout(httpParameters, connectionTimeout);
+        // Set the default socket timeout (SO_TIMEOUT)
+        // in milliseconds which is the timeout for waiting for data.
+        HttpConnectionParams.setSoTimeout(httpParameters, socketTimeout);
 
-	final HttpParams httpParameters = new BasicHttpParams();
-	// Set the timeout in milliseconds until a connection is established.
-	HttpConnectionParams.setConnectionTimeout(httpParameters, connectionTimeout);
-	// Set the default socket timeout (SO_TIMEOUT)
-	// in milliseconds which is the timeout for waiting for data.
-	HttpConnectionParams.setSoTimeout(httpParameters, socketTimeout);
+        getClient().setParams(httpParameters);
+        HttpResponse httpResponse;
+        InputStream instream = null;
+        try {
 
-	getClient().setParams(httpParameters);
-	HttpResponse httpResponse;
-	InputStream instream = null;
-	try {
+            httpResponse = getClient().execute(request);
 
-	    httpResponse = getClient().execute(request);
+            responseStatus.setStatusCode(httpResponse.getStatusLine().getStatusCode());
+            responseStatus.setStatusMessage(httpResponse.getStatusLine().getReasonPhrase());
 
-	    responseStatus.setStatusCode(httpResponse.getStatusLine().getStatusCode());
-	    responseStatus.setStatusMessage(httpResponse.getStatusLine().getReasonPhrase());
+            final HttpEntity entity = httpResponse.getEntity();
 
-	    final HttpEntity entity = httpResponse.getEntity();
+            if (entity != null) {
 
-	    if (entity != null) {
+                instream = entity.getContent();
+                response = StreamUtil.convertStreamToString(instream);
+                Log.d(TAG, "URL: " + url + " RESPONSE: " + response);
+                // Closing the input stream will trigger connection release
+                instream.close();
+            }
 
-		instream = entity.getContent();
-		response = StreamUtil.convertStreamToString(instream);
-		Log.d(TAG, "URL: " + url + " RESPONSE: " + response);
-		// Closing the input stream will trigger connection release
-		instream.close();
-	    }
-
-	} catch (final IOException e) {
-	    throw e;
-	} catch (final RuntimeException ex) {
-	    // In case of an unexpected exception you may want to abort
-	    // the HTTP request in order to shut down the underlying
-	    // connection immediately.
-	    request.abort();
-	    throw ex;
-	} finally {
-	    // Closing the input stream will trigger connection release
-	    StreamUtil.silentClose(instream);
-	}
+        } catch (final IOException e) {
+            throw e;
+        } catch (final RuntimeException ex) {
+            // In case of an unexpected exception you may want to abort
+            // the HTTP request in order to shut down the underlying
+            // connection immediately.
+            request.abort();
+            throw ex;
+        } finally {
+            // Closing the input stream will trigger connection release
+            StreamUtil.silentClose(instream);
+        }
     }
 
     public static DefaultHttpClient getClient() {
-	if (client == null) {
-	    client = StreamUtil.generateClient();
-	}
-	return client;
+        if (client == null) {
+            client = StreamUtil.generateClient();
+        }
+        return client;
     }
 
     public static String generateParametersString(final ArrayList<NameValuePair> params)
-	    throws UnsupportedEncodingException {
-	// add parameters
-	String combinedParams = "";
-	if (params != null && !params.isEmpty()) {
-	    combinedParams += "?";
-	    for (final NameValuePair p : params) {
-		final String paramString = p.getName() + "="
-			+ URLEncoder.encode(p.getValue(), "UTF-8");
-		if (combinedParams.length() > 1) {
-		    combinedParams += "&" + paramString;
-		} else {
-		    combinedParams += paramString;
-		}
-	    }
-	}
-	return combinedParams;
+            throws UnsupportedEncodingException {
+        // add parameters
+        String combinedParams = "";
+        if (params != null && !params.isEmpty()) {
+            combinedParams += "?";
+            for (final NameValuePair p : params) {
+                final String paramString = p.getName() + "="
+                        + URLEncoder.encode(p.getValue(), "UTF-8");
+                if (combinedParams.length() > 1) {
+                    combinedParams += "&" + paramString;
+                } else {
+                    combinedParams += paramString;
+                }
+            }
+        }
+        return combinedParams;
     }
 
 }
--- rest-client-lib/src/com/dg/libs/rest/requests/ParameterHttpRequestImpl.java
+++ rest-client-lib/src/com/dg/libs/rest/requests/ParameterHttpRequestImpl.java
@@ -15,24 +15,24 @@ public abstract class ParameterHttpRequestImpl<T> extends BaseHttpRequestImpl<T>
     protected ParametersRestClient client;
 
     public ParameterHttpRequestImpl(final Context context, final RequestMethod requestMethod,
-	    final HttpResponseParser<T> parser, final HttpCallback<T> callback) {
-	super(context, parser, callback);
-	client = new ParametersRestClient();
-	client.setMethod(requestMethod);
+            final HttpResponseParser<T> parser, final HttpCallback<T> callback) {
+        super(context, parser, callback);
+        client = new ParametersRestClient();
+        client.setMethod(requestMethod);
     }
 
     @Override
     protected void prepareAndExecuteRequest() throws HttpException {
-	prepareParams();
-	client.execute();
+        prepareParams();
+        client.execute();
     }
 
     @Override
     public Rest getClient() {
-	return client;
+        return client;
     }
 
     public void addParam(final String key, final String value) {
-	client.addParam(key, value);
+        client.addParam(key, value);
     }
 }
